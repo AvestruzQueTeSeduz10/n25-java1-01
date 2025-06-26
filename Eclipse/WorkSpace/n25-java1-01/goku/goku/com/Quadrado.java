dd24e28d438478c67acffb13d447abeaf5b930d5
joao pedro brandao baptista <joaobolinhafc@gmail.com>
2025-06-25 21:52:33 -0300

    aula 10 eclipse ult aula

---
+++ Eclipse/WorkSpace/n25-java1-01/goku/goku/com/Quadrado.java
@@ -0,0 +1,16 @@
+package goku.com;
+
+import java.awt.Color;
+
+public class Quadrado extends Retangulo {
+
+	
+	public Quadrado(int posX, int posY, int larg, Color cor) {
+		super(posX, posY, larg, larg, cor);
+		// TODO Auto-generated constructor stub
+	}
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
---
+++ Eclipse/WorkSpace/n25-java1-01/goku/goku/e.java
@@ -0,0 +1,5 @@
+package goku;
+
+public class e {
+
+}
